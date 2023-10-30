2e03eefa48602dc51b6adb5f267cf768630e4b5b
LeonCotten <18023812-LeonCotten@users.noreply.replit.com>
2023-10-30 19:44:30 +0000

    Optimized terminal by using switch statements

--- OrchiDB/terminal.js
+++ OrchiDB/terminal.js
@@ -3,80 +3,100 @@ const init = require('./init.js');
 const prompt = require('prompt-sync')();
 let db = '';
 let dbInfo = '';
-const start = () =>{
+
+const start = () => {
   let command = prompt('>>');
-  if(command == 'EXIT'){
-    console.log('<<Exiting Terminal');
-  }else if(command == 'ENTER_PASS'){
-    const pass = prompt('<<Enter Password: \n>>');
-    db.enterPass(pass);
-    start();
-  }else if(command == 'INIT_DB'){
-    const dbName = prompt('<<Enter Database Name: \n>>');
-    const dbPass = prompt('<<Enter Database Password: \n>>');
-    init.db(dbName, dbPass);
-    start();
-  }else if(command == 'SELECT_DB'){
-    const dbName = prompt('<<Enter Database Name: \n>>');
-    db = require(path.join(__dirname, dbName, 'db.js'));
-    dbInfo = require(path.join(__dirname, dbName, 'dbInfo.json'));
-    console.log(`<<Database ${dbName} Selected`);
-    start();
-  }else if(command == 'NEW_DOC'){
-    const docName = prompt('<<Enter Doc Name: \n>>');
-    db.newDoc(docName);
-    start();
-  }else if(command == 'READ_DOC'){
-    const docName = prompt('<<Enter Doc Name: \n>>');
-    db.readDoc(docName);
-    start();
-  }else if(command == 'WRITE_DOC'){
-    const docName = prompt('<<Enter Doc Name: \n>>');
-    const content = prompt('<<Enter Content: \n>>');
-    db.writeDoc(docName, content);
-    start();
-  }else if(command == 'DELETE_DOC'){
-    const docName = prompt('<<Enter Doc Name: \n>>');
-    db.deleteDoc(docName);
-    start();
-  }else if(command == 'RESTORE_DOC'){
-    const docName = prompt('<<Enter Doc Name: \n>>');
-    db.restoreDoc(docName);
-    start();
-  }else if(command == 'EMPTY_TRASH'){
-    db.emptyTrash();
-    start();
-  }else if(command == 'RENAME_DOC'){
-    const docName = prompt('<<Enter Doc Name: \n>>');
-    const newName = prompt('<<Enter New Doc Name: \n>>');
-    db.renameDoc(docName, newName);
-    start();
-  }else if(command == 'RETURN_PATH'){
-    const docName = prompt('<<Enter Doc Name: \n>>');
-    db.returnPath(docName);
-    start();
-  }else if(command == 'COPY_DB'){
-    const dbName = prompt('<<Enter Database Name: \n>>');
-    const dbPass = prompt('<<Enter Database Password: \n>>');
-    db.copyDB(dbName, dbPass);
-    start();
-  }else if(command == 'DEL_DB'){
-    const confirm = prompt(`<<Are you sure you want to delete this database (${dbInfo.dbName}) (Y/N)\n>>`);
-    const dbPass = prompt('<<Enter Database Password: \n>>');
-    db.deleteDbWithoutTimeout(dbPass, confirm);
-    start();
-  }else if(command == 'CLEAR'){
-    console.clear();
-    start();
-  }else if(command == 'EDIT_DOC'){
-    const docName = prompt("<<Enter Doc Name: \n>>");
-    const variable = prompt("<<Enter Variable Name: \n>>");
-    const value = prompt("<<Enter Value: \n>>");
-    db.editDoc(docName, variable, value);
-    start();
-  }else{
-    console.log('<<Invalid Command');
-    start();
+
+  switch (command) {
+    case 'EXIT':
+      console.log('<<Exiting Terminal');
+      break;
+    case 'ENTER_PASS':
+      const pass = prompt('<<Enter Password: \n>>');
+      db.enterPass(pass);
+      start();
+      break;
+    case 'INIT_DB':
+      const dbName = prompt('<<Enter Database Name: \n>>');
+      const dbPass = prompt('<<Enter Database Password: \n>>');
+      init.db(dbName, dbPass);
+      start();
+      break;
+    case 'SELECT_DB':
+      const selectDbName = prompt('<<Enter Database Name: \n>>');
+      db = require(path.join(__dirname, selectDbName, 'db.js'));
+      dbInfo = require(path.join(__dirname, selectDbName, 'dbInfo.json'));
+      console.log(`<<Database ${selectDbName} Selected`);
+      start();
+      break;
+    case 'NEW_DOC':
+      const newDocName = prompt('<<Enter Doc Name: \n>>');
+      db.newDoc(newDocName);
+      start();
+      break;
+    case 'READ_DOC':
+      const readDocName = prompt('<<Enter Doc Name: \n>>');
+      db.readDoc(readDocName);
+      start();
+      break;
+    case 'WRITE_DOC':
+      const writeDocName = prompt('<<Enter Doc Name: \n>>');
+      const content = prompt('<<Enter Content: \n>>');
+      db.writeDoc(writeDocName, content);
+      start();
+      break;
+    case 'DELETE_DOC':
+      const deleteDocName = prompt('<<Enter Doc Name: \n>>');
+      db.deleteDoc(deleteDocName);
+      start();
+      break;
+    case 'RESTORE_DOC':
+      const restoreDocName = prompt('<<Enter Doc Name: \n>>');
+      db.restoreDoc(restoreDocName);
+      start();
+      break;
+    case 'EMPTY_TRASH':
+      db.emptyTrash();
+      start();
+      break;
+    case 'RENAME_DOC':
+      const renameDocName = prompt('<<Enter Doc Name: \n>>');
+      const newName = prompt('<<Enter New Doc Name: \n>>');
+      db.renameDoc(renameDocName, newName);
+      start();
+      break;
+    case 'RETURN_PATH':
+      const returnPathName = prompt('<<Enter Doc Name: \n>>');
+      db.returnPath(returnPathName);
+      start();
+      break;
+    case 'COPY_DB':
+      const copyDbName = prompt('<<Enter Database Name: \n>>');
+      const copyDbPass = prompt('<<Enter Database Password: \n>>');
+      db.copyDB(copyDbName, copyDbPass);
+      start();
+      break;
+    case 'DEL_DB':
+      const confirm = prompt(`<<Are you sure you want to delete this database (${dbInfo.dbName}) (Y/N)\n>>`);
+      const deleteDbPass = prompt('<<Enter Database Password: \n>>');
+      db.deleteDbWithoutTimeout(deleteDbPass, confirm);
+      start();
+      break;
+    case 'CLEAR':
+      console.clear();
+      start();
+      break;
+    case 'EDIT_DOC':
+      const editDocName = prompt("<<Enter Doc Name: \n>>");
+      const variable = prompt("<<Enter Variable Name: \n>>");
+      const value = prompt("<<Enter Value: \n>>");
+      db.editDoc(editDocName, variable, value);
+      start();
+      break;
+    default:
+      console.log('<<Invalid Command');
+      start();
+      break;
   }
-}
+};
 module.exports = { start };
