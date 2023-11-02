a3cdcab573e45100bb78de375a6e7706dfbb5f29
LeonCotten <18023812-LeonCotten@users.noreply.replit.com>
2023-11-02 12:11:16 +0000

    Edited Divider

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-console.log('$-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_');
+console.log('$-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-');
 console.log('$                OrchiDB             ');
-console.log('$-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_');
+console.log('$-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-');
 console.log('$ ');
 console.log('$ ');
 console.log('$ Welcome to OrchiDB. A lightweight, document-oriented database managment system for JavaScript.');
@@ -8,4 +8,4 @@ console.log('$ For more information, read the README.md file in the source code.
 console.log('$ To use this database, copy the files in the OrchiDB folder or fork the code to your own project.');
 console.log('$ ');
 console.log('$ ');
-console.log('$-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_');
+console.log('$-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-');
