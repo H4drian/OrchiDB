6581dc55cdc7511dcff78a934bce6e1aabed0b48
LeonCotten <18023812-LeonCotten@users.noreply.replit.com>
2023-11-03 01:59:29 +0000

    Added title file

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-const terminal = require('./OrchiDB/terminal.js');
-terminal.start();
+console.log('|-------------------------------------------------|');
+console.log('|                     OrchiDB                     |');
+console.log('|-------------------------------------------------|');
+console.log('|                Welcome to OrchiDB               |');
+console.log('|          A lightweight Document Oriented        |');
+console.log('|      Database Management System for JavaScript  |');
+console.log('|-------------------------------------------------|');
+console.log('| OrchiDB is a Node.js package that allows you to |');
+console.log('| Manage Data easily through text based input,    |');
+console.log('| QL and Javascript. For more information please  |');
+console.log('| visit https://github.com/OrchiDB/OrchiDB or     |');
+console.log('| read the markdown file inside this replit.      |');
+console.log('|-------------------------------------------------|');
