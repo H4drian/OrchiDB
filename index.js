84f8bbf3fbf13ce3ee8841173594548687490658
H4drian <117495728+H4drian@users.noreply.github.com>
2023-11-03 11:40:36 +0000

    Added link to github repo
    real

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ console.log('|-------------------------------------------------|');
 console.log('| OrchiDB is a Node.js package that allows you to |');
 console.log('| Manage Data easily through text based input,    |');
 console.log('| QL and Javascript. For more information please  |');
-console.log('| visit https://github.com/OrchiDB/OrchiDB or     |');
+console.log('| visit https://github.com/H4drian/OrchiDB or     |');
 console.log('| read the markdown file inside this replit.      |');
 console.log('| OrchiDB is still in early development so expect |');
 console.log('| some errors.                                    |');
