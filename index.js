bf2c996a073367a28784c442c5db361fab3fc688
H4drian <117495728+H4drian@users.noreply.github.com>
2023-11-09 14:24:02 +0000

    nvm had to delete myCollection.

--- index.js
+++ index.js
@@ -15,7 +15,3 @@ console.log('| some errors.                                    |');
 console.log('|-------------------------------------------------|');
 console.log('|    Developed by H4    |    DB Version: 2        |');
 console.log('|-------------------------------------------------|');
-
-const db  = require('./OrchiDB/orchidb.js');
-const myCollection = new db.Collection('myCollection');
-myCollection.terminal.run();
